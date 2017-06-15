f9eca3ce53b0fa74047e724660717cb15b77c4c3
logonrm <logonrm@fiap.com.br>
2017-06-14 21:38:38 -0300

    stetho

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,15 @@
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by logonrm on 14/06/2017.
+ */
+
+public class MyApplication extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
